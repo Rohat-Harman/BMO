7bd6f230d87d003ea376163ae1638b532bf44032
Rohat <rohatharman34@gmail.com>
2026-07-01 14:27:31 +0300

    add the futere plan of speak function

--- voice/speaker.py
+++ voice/speaker.py
@@ -1,2 +1,13 @@
+from tkinter.filedialog import test
+
+
 def speak(test):
+
+    """
+    Makes BMO speak.
+
+    For now, it prints text.
+    Later, this function will generate BMO's voice.
+
+    """
     print(test)
