be860b634db98f7811f49161fb69642a27312428
Rohat <rohatharman34@gmail.com>
2026-07-12 12:46:11 +0300

    Prepare Kokoro engine

--- voice/kokoro_engine.py
+++ voice/kokoro_engine.py
@@ -5,11 +5,11 @@ MODEL_PATH = "models/tts/kokoro-v1.0.onnx"
 VOICES_PATH = "models/tts/voices-v1.0.bin"
 
 
-def test_voice():
+def generate_voice(text):
     kokoro = Kokoro(MODEL_PATH, VOICES_PATH)
 
     samples, sample_rate = kokoro.create(
-        "Hello friend! BMO is awake!",
+        text,
         voice="af_sarah",
         speed=1.0,
         lang="en-us"
