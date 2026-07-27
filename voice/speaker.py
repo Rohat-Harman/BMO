aedc3f7c0d48eebfb2e3f519d5d9d4cd77e4206c
Rohat Harman <rohatharman34@gmail.com>
2026-07-27 15:22:07 +0300

    uptade speaker description

--- voice/speaker.py
+++ voice/speaker.py
@@ -1,12 +1,11 @@
+from pydoc import text
+
 from voice.kokoro_engine import generate_voice as generate_english_voice
 
 def speak(text, language="en"):
 
+   """
+    Makes BMO speak using the Kokoro voice engine.
     """
-    Makes BMO speak.
 
-    For now, it prints text.
-    Later, this function will generate BMO's voice.
-
-    """
-    generate_english_voice(text)
+generate_english_voice(text)
